a7bcc9aafa659884835d39cdedcb80ded1d5f625
Craig McKeachie <craigdmckeachie@gmail.com>
2025-06-17 09:15:50 -0400

    add product dao interface and simple implementation

--- northwindtraders.console/src/main/java/com/pluralsight/northwindtraders/console/Application.java
+++ northwindtraders.console/src/main/java/com/pluralsight/northwindtraders/console/Application.java
@@ -1,11 +1,16 @@
 package com.pluralsight.northwindtraders.console;
 
+import com.pluralsight.northwindtraders.console.dao.ProductDao;
+import com.pluralsight.northwindtraders.console.dao.SimpleProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+	@Autowired
+	SimpleProductDao productDao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
